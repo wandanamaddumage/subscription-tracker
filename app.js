70b74cd91d4f17423a9a4cf6546c01967a8aaa96
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-04-09 23:16:42 +0530

    feat: add error handler

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import { PORT } from "./config/env.js";
 
@@ -6,13 +7,20 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscrption.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import erroMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser);
 
 // api/v1/auth/sign-up
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/subscrptions", subscriptionRouter);
+
+app.use(erroMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API!");
