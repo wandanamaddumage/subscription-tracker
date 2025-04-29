1ed8236d72e18e8fa79133f803ed494c7aa83047
Wandana Maddumage <121846047+wandanamaddumage@users.noreply.github.com>
2025-04-29 22:24:02 +0530

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,7 @@ import { signIn, signUp, signOut } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
-// Path: /api/v1/auth/sign-up (POST)
+// Path: /api/v1/auth/sign-up (POST).
 authRouter.post("/sign-up", signUp);
 authRouter.post("/sign-in", signIn);
 authRouter.post("/sign-out", signOut);
